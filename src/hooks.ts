b965b371e517689d07317c6828f12efa7076ec35
Daniel Hallgren <1824229+danielpox@users.noreply.github.com>
2020-06-15 12:25:26 +0200

    Change to localStorage from sessionStorage

--- src/hooks.ts
+++ src/hooks.ts
@@ -4,7 +4,7 @@ import { deserializeListItems, serializeListItems } from './storage'
 
 export function useStickyState (defaultValue: IListItem[], key: string): [IListItem[], (item: IListItem[]) => void] {
   const [ value, setValue ] = useState<IListItem[]>(() => {
-    const stickyValue = window.sessionStorage.getItem(key)
+    const stickyValue = window.localStorage.getItem(key)
 
     if (stickyValue === null) return defaultValue
 
@@ -17,7 +17,7 @@ export function useStickyState (defaultValue: IListItem[], key: string): [IListI
   useEffect(() => {
     const serializedValue = serializeListItems(value)
 
-    window.sessionStorage.setItem(key, JSON.stringify(serializedValue))
+    window.localStorage.setItem(key, JSON.stringify(serializedValue))
   }, [key, value])
 
   return [ value, setValue ]
